326c3d69f1465e9a09fd6a808ad04959deb13ea2
lensonlovescode <lensonisinparis@gmail.com>
2024-12-05 21:42:59 +0300

    betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,35 +7,34 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *right_node;
+	binary_tree_t *right_node;
 
-        if (parent == NULL)
-        {
-                return (NULL);
-        }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-		right_node = binary_tree_node(parent, value);
+	right_node = binary_tree_node(parent, value);
 
-        if (right_node == NULL)
-        {
-                return (NULL);
-        }
+	if (right_node == NULL)
+	{
+		return (NULL);
+	}
 
-        right_node->parent = parent;
-        right_node->n = value;
-        right_node->left = NULL;
+	right_node->parent = parent;
+	right_node->n = value;
+	right_node->left = NULL;
 
-        if (parent->right == NULL)
-        {
-                right_node->right = NULL;
-                parent->right = right_node;
-        }
-        else
-        {
-                right_node->right = parent->right;
-                parent->right->parent = right_node;
-                parent->right = right_node;
-        }
-        return (right_node);
-
+	if (parent->right == NULL)
+	{
+		right_node->right = NULL;
+		parent->right = right_node;
+	}
+	else
+	{
+		right_node->right = parent->right;
+		parent->right->parent = right_node;
+		parent->right = right_node;
+	}
+	return (right_node);
 }
